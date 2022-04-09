08cf2bb1358ef9c7020f0af0905d71f274dc7ef4
minkim94 <alswl8671@gmail.com>
2022-04-09 18:06:53 +0900

    새 컴포넌트 생성

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+// 데코레이터 영역 : 컴포넌트 생성에 필요한 정보를 담고있는 객체를 인자로 전달 (메타데이터 객체)
+// 컴포넌트는 하나의 레고 조각이라고 볼 수 있다.
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-root', // 컴포넌트의 태그네임 (index.html의 <app-root></app-root>)
+  templateUrl: './app.component.html', // 컴포넌트가 어떻게 렌더링될지 정의 하기 위함. 직접 작성할 수도 있고 파일을 불러올 수도 있음.
+  styleUrls: ['./app.component.scss'] // css 파일
 })
+
 export class AppComponent {
   title = 'front-end';
 }
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+
+/* main.ts 에서 AppModule 을 실행하고, AppModule 에서 AppComponent를 부팅한다. */
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // 컴포넌트들을 담는 곳
+    AppComponent,
+    HelloComponent
   ],
-  imports: [
+  imports: [ // 모듈들을 담는 곳
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], // 뷰가 없는(컴포넌트가 아닌) 서비스 로직들을 담는 곳
+  bootstrap: [AppComponent] // 처음 실행할 컴포넌트를 지정하는 곳
 })
 export class AppModule { }
